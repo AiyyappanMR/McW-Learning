869c5aba564213af48465dddb3598752dab3c89b
AiyyappanMR <aiyyappan2004@gmail.com>
2026-03-06 15:30:35 +0530

    basic cmake project

---
+++ Day11 (06-03-26)/Basic_Cmake/build/CMakeFiles/hellowrld.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellowrld.dir/hellowrld.cpp.o"
+  "CMakeFiles/hellowrld.dir/hellowrld.cpp.o.d"
+  "hellowrld"
+  "hellowrld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hellowrld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
