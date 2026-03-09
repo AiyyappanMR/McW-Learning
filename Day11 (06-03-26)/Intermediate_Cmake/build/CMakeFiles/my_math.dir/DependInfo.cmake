79a7ec06942332e1dad23f6a42b3f81f5a4f6c39
AiyyappanMR <aiyyappan2004@gmail.com>
2026-03-09 11:24:02 +0530

    subdirectory using main cmakelist

---
+++ Day11 (06-03-26)/Intermediate_Cmake/build/CMakeFiles/my_math.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mcw/Desktop/McW-Learning/Day11 (06-03-26)/Intermediate_Cmake/user_lib/my_math/my_math.cpp" "CMakeFiles/my_math.dir/user_lib/my_math/my_math.cpp.o" "gcc" "CMakeFiles/my_math.dir/user_lib/my_math/my_math.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
