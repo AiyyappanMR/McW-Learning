f371c65fad67f5a8e65c29311e4ef9e496443230
AiyyappanMR <aiyyappan2004@gmail.com>
2026-03-12 12:03:02 +0530

    Untrack build folders and update .gitignore

--- Day11 (06-03-26)/Cmake_lib_one_cmakelist/build/CMakeFiles/my_math.dir/user_lib/my_math/my_math.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/my_math.dir/user_lib/my_math/my_math.cpp.o: \
- /home/mcw/Desktop/McW-Learning/Day11\ (06-03-26)/Intermediate_Cmake/user_lib/my_math/my_math.cpp \
- /usr/include/stdc-predef.h \
- /home/mcw/Desktop/McW-Learning/Day11\ (06-03-26)/Intermediate_Cmake/user_lib/my_math/my_math.hpp
